37fe57c4caf39bc28deeaf9a3b023d43b636ac29
irina-dzikun <irina.dzikun@gmail.com>
2023-06-23 23:32:44 +0200

    Cod refactoring

--- src/components/contexts/quiz.js
+++ src/components/contexts/quiz.js
@@ -14,41 +14,43 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-  if (action.type === "SELECT_ANSWER") {
-    const correctAnswersCount =
-      action.payload ===
-      state.questions[state.currentQuestionIndex].correctAnswer
-        ? state.correctAnswersCount + 1
-        : state.correctAnswersCount;
-    return {
-      ...state,
-      currentAnswer: action.payload,
-      correctAnswersCount,
-    };
+  switch (action.type) {
+    case "SELECT_ANSWER": {
+      const correctAnswersCount =
+        action.payload ===
+        state.questions[state.currentQuestionIndex].correctAnswer
+          ? state.correctAnswersCount + 1
+          : state.correctAnswersCount;
+      return {
+        ...state,
+        currentAnswer: action.payload,
+        correctAnswersCount,
+      };
+    }
+    case "NEXT_QUESTION": {
+      const showResults =
+        state.currentQuestionIndex === state.questions.length - 1;
+      const currentQuestionIndex = showResults
+        ? state.currentQuestionIndex
+        : state.currentQuestionIndex + 1;
+      const answers = showResults
+        ? []
+        : shuffleAnswers(state.questions[currentQuestionIndex]);
+      return {
+        ...state,
+        currentQuestionIndex,
+        showResults,
+        answers,
+        currentAnswer: "",
+      };
+    }
+    case "RESTART": {
+      return initialState;
+    }
+    default: {
+      return state;
+    }
   }
-  if (action.type === "NEXT_QUESTION") {
-    const showResults =
-      state.currentQuestionIndex === state.questions.length - 1;
-    const currentQuestionIndex = showResults
-      ? state.currentQuestionIndex
-      : state.currentQuestionIndex + 1;
-    const answers = showResults
-      ? []
-      : shuffleAnswers(state.questions[currentQuestionIndex]);
-    return {
-      ...state,
-      currentQuestionIndex,
-      showResults,
-      answers,
-      currentAnswer: "",
-    };
-  }
-
-  if (action.type === "RESTART") {
-    return initialState;
-  }
-
-  return state;
 };
 
 export const QuizProvider = ({ children }) => {
