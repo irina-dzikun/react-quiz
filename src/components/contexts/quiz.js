fb7b227d44e5c9a52a7ef2e80f49b392f4ad096a
irina-dzikun <irina.dzikun@gmail.com>
2023-06-23 13:24:10 +0200

    Implement next button logic for questions

--- src/components/contexts/quiz.js
+++ src/components/contexts/quiz.js
@@ -1,7 +1,24 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import data from "../../data";
 
 export const QuizContext = createContext();
 
+const initialState = {
+  currentQuestionIndex: 0,
+  questions: data,
+};
+
+const reducer = (state, action) => {
+  if (action.type === "NEXT_QUESTION") {
+    return {
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    };
+  }
+  return state;
+};
+
 export const QuizProvider = ({ children }) => {
-  return <QuizContext.Provider>{children}</QuizContext.Provider>;
+  const value = useReducer(reducer, initialState);
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
