e83799a0465ecd8e14adc9a266b08b120896a76a
irina-dzikun <irina.dzikun@gmail.com>
2023-06-23 14:11:49 +0200

    Implement results page layout and restart button logic

--- src/components/contexts/quiz.js
+++ src/components/contexts/quiz.js
@@ -6,15 +6,27 @@ export const QuizContext = createContext();
 const initialState = {
   currentQuestionIndex: 0,
   questions: data,
+  showResults: false,
 };
 
 const reducer = (state, action) => {
   if (action.type === "NEXT_QUESTION") {
+    const showResults =
+      state.currentQuestionIndex === state.questions.length - 1;
+    const currentQuestionIndex = showResults
+      ? state.currentQuestionIndex
+      : state.currentQuestionIndex + 1;
     return {
       ...state,
-      currentQuestionIndex: state.currentQuestionIndex + 1,
+      currentQuestionIndex,
+      showResults,
     };
   }
+
+  if (action.type === "RESTART") {
+    return initialState;
+  }
+
   return state;
 };
 
