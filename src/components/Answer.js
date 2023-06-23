45390d0b2fe3714faaea1b2527563fea85702fbb
irina-dzikun <irina.dzikun@gmail.com>
2023-06-23 23:20:44 +0200

    Implement answers logic

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,8 +1,24 @@
-const Answer = () => {
+const Answer = ({
+  answerText,
+  index,
+  onSelectAnswer,
+  correctAnswer,
+  currentAnswer,
+}) => {
+  const letterMapping = ["A", "B", "C", "D"];
+  const isCorrectAnswer = currentAnswer && correctAnswer === answerText;
+  const isWrongAnswer =
+    currentAnswer === answerText && currentAnswer !== correctAnswer;
+  const correctAnswerClass = isCorrectAnswer ? "correct-answer" : "";
+  const currentAnswerClass = isWrongAnswer ? "wrong-answer" : "";
+  const disabledClass = currentAnswer ? "disabled-answer" : "";
   return (
-    <div className="answer">
-      <div className="answer-letter">A</div>
-      <div className="answer-text">Text of answer</div>
+    <div
+      className={`answer ${correctAnswerClass} ${currentAnswerClass} ${disabledClass}`}
+      onClick={() => onSelectAnswer(answerText)}
+    >
+      <div className="answer-letter">{letterMapping[index]}</div>
+      <div className="answer-text">{answerText}</div>
     </div>
   );
 };
--- src/components/contexts/quiz.js
+++ src/components/contexts/quiz.js
@@ -1,25 +1,46 @@
 import { createContext, useReducer } from "react";
-import data from "../../data";
+import questions from "../../data";
+import { shuffleAnswers } from "../../helpers";
 
 export const QuizContext = createContext();
 
 const initialState = {
   currentQuestionIndex: 0,
-  questions: data,
+  questions,
   showResults: false,
+  answers: shuffleAnswers(questions[0]),
+  currentAnswer: "",
+  correctAnswersCount: 0,
 };
 
 const reducer = (state, action) => {
+  if (action.type === "SELECT_ANSWER") {
+    const correctAnswersCount =
+      action.payload ===
+      state.questions[state.currentQuestionIndex].correctAnswer
+        ? state.correctAnswersCount + 1
+        : state.correctAnswersCount;
+    return {
+      ...state,
+      currentAnswer: action.payload,
+      correctAnswersCount,
+    };
+  }
   if (action.type === "NEXT_QUESTION") {
     const showResults =
       state.currentQuestionIndex === state.questions.length - 1;
     const currentQuestionIndex = showResults
       ? state.currentQuestionIndex
       : state.currentQuestionIndex + 1;
+    const answers = showResults
+      ? []
+      : shuffleAnswers(state.questions[currentQuestionIndex]);
     return {
       ...state,
       currentQuestionIndex,
       showResults,
+      answers,
+      currentAnswer: "",
     };
   }
 
