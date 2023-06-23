a0854d7c2e068e76ffda5607ce4ea64eece1e338
irina-dzikun <irina.dzikun@gmail.com>
2023-06-23 12:45:25 +0200

    Add createContext

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { QuizProvider } from "./components/contexts/quiz";
 import Quiz from "./components/Quiz";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Quiz />
+    <QuizProvider>
+      <Quiz />
+    </QuizProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
